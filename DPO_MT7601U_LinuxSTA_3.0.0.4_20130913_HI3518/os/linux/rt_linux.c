b931e31f4e2b979b47a64a50570e95a31edd40b4
pjg <pjg864@gmail.com>
2015-09-18 21:21:29 +0800

    merge DPO_MT7601U_LinuxSTA_3.0.0.4_20130913 platform to DPO_MT7601U_LinuxSTA_3.0.0.4_20130913_HI3518

--- DPO_MT7601U_LinuxSTA_3.0.0.4_20130913_HI3518/os/linux/rt_linux.c
+++ DPO_MT7601U_LinuxSTA_3.0.0.4_20130913_HI3518/os/linux/rt_linux.c
@@ -1117,9 +1117,12 @@ static inline void __RtmpOSFSInfoChange(OS_FS_INFO * pOSFSInfo, BOOLEAN bSet)
 		pOSFSInfo->fsuid = current->fsuid;
 		pOSFSInfo->fsgid = current->fsgid;
 		current->fsuid = current->fsgid = 0;
-#else
+#elif LINUX_VERSION_CODE < KERNEL_VERSION(3,12,0)
 		pOSFSInfo->fsuid = current_fsuid();
 		pOSFSInfo->fsgid = current_fsgid();
+#else
+		pOSFSInfo->fsuid = current_fsuid().val;
+		pOSFSInfo->fsgid = current_fsgid().val;
 #endif
 		pOSFSInfo->fs = get_fs();
 		set_fs(KERNEL_DS);
